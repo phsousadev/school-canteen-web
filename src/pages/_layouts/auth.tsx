0ac7a9f4f78efb2fe269e8f984c3ed0fe8a25f07
Pedro Sousa <pedro.sousa@divam.com.br>
2025-05-21 04:06:06 -0300

    style: configurind layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,21 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">school.canteen</span>
+        </div>
 
-      <div>
+        <footer className="text-sm">
+          Painel &copy; school.canteen - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>

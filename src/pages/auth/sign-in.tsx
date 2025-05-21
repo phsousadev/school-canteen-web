639a6a1763d35640bd5fad4ffe8728c93f95b147
Pedro Sousa <pedro.sousa@divam.com.br>
2025-05-21 04:51:50 -0300

    feat: user registration

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -8,10 +8,11 @@ import { Input } from '@/components/ui/input'
 
 import { z } from 'zod'
 import { toast } from 'sonner'
+import { Link } from 'react-router-dom'
 
 const signInform = z.object({
   email: z.string().email(),
-  password: z.string().min(6)
+  password: z.string()
 })
 
 type SignInForm = z.infer<typeof signInform>
@@ -32,6 +33,10 @@ export function SignIn() {
       <Helmet title="Login" />
 
       <div className="p-8">
+        <Button variant="outline" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo cadastro</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,111 @@
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signUpForm = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  phone: z.string(),
+  password: z.string().min(6),
+})
+
+type SignUpForm = z.infer<typeof signUpForm>
+
+export function SignUp() {
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignUpForm>()
+
+  async function handleSignUp(data: SignUpForm) {
+    try {
+      console.log(data)
+
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Usuário cadastrado com sucesso!', {
+        action: {
+          label: 'Login',
+          onClick: () => navigate('/sign-in'),
+        },
+      })
+    } catch (error) {
+      toast.error('Erro ao cadastrar usuário.')
+    }
+  }
+
+  return (
+    <>
+      <Helmet title="Cadastro" />
+
+      <div className="p-8">
+        <Button variant="outline" asChild className="absolute right-8 top-8">
+          <Link to="/sign-in">Fazer login</Link>
+        </Button>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Criar conta
+            </h1>
+          </div>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
+            <div className="space-y-2">
+              <Label htmlFor="name">Nome</Label>
+              <Input
+                id="name"
+                type="text"
+                {...register('name')}
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">E-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="phone">Celular</Label>
+              <Input id="phone" type="tel" {...register('phone')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Senha</Label>
+              <Input
+                id="password"
+                type="password"
+                autoComplete="current-password"
+                required
+              />
+            </div>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Finalizar cadastro
+            </Button>
+
+            <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
+              Ao continuar, você concorda com nossos{' '}
+              <a href="" className="underline underline-offset-4">
+                termos de serviço
+              </a>{' '}
+              e{' '}
+              <a href="" className="underline underline-offset-4">
+                políticas de privacidade
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}

baa3a88803c36116570bea5b93370425f3998136
Pedro Sousa <pedro.sousa@divam.com.br>
2025-05-21 05:18:20 -0300

    feat: thema toggle

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { router } from './routes'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey='schoolcanteen-theme' defaultTheme='dark'>
+        <Helmet titleTemplate="%s | school.canteen" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
